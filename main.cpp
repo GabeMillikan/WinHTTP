637ca941043a4adbb02a3e998405e4419d7d1103
ParticleChurch <70173558+ParticleChurch@users.noreply.github.com>
2020-11-30 20:44:21 -0800

    updated example usage

--- main.cpp
+++ main.cpp
@@ -8,9 +8,6 @@ struct pastebin
 int main()
 {
 	std::string out;
-	bool success = HTTP::Post(
-		"https://httpdump.io/fcxgn", //"https://pastebin.com/api/api_post.php",
-	"api_dev_key=glerPifRdWtMqYW5FIUMwqn6lZrir5CC&api_option=paste&api_paste_private=0&api_paste_name=myname.js&api_paste_expire_date=10M&api_paste_format=javascript&api_paste_code=ayoo", out);
-
+	bool success = HTTP::Post("https://www.example.com/api/example.php", "the data to post", out);
 	std::cout << "output: " << out << std::endl;
 }
